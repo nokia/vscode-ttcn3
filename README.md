85aaf52e8290dd582d12eb234f9c88dbaeb35e1d
Matthias Simon <matthias.simon@nokia.com>
2020-04-16 10:27:42 +0200

    Various fixes

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # TTCN-3 for Visual Studio Code
 
-This will be the vscode extension for TTCN-3 support. There's not much to see yet.
+Adds language support for TTCN-3 to Visual Studio Code.  This extension is a fork of [Emmanuel Alap's TTCN-3 Language Extension](https://github.com/ealap/vscode-language-ttcn) and adds further smartness. Current features:
 
+* Jump to Definition
+* Snippets
+* Syntax Highlighting
+
+
+TTCN-3 support is powered by the [NTT/K3 Language Server](http://github.com/nokia/ntt). Please make sure you have it installed, because this extension cannot install `k3` for you, yet.
+
+
+## Contribution
+
+We are pushing hard to get to you as many features as soon as possible, but
+there's still much to do in every corner. Help is very much appreciated. 
--- src/extension.ts
+++ src/extension.ts
@@ -1,41 +1,67 @@
 import * as path from 'path';
 import * as vscode from 'vscode';
-import { workspace, ExtensionContext } from 'vscode';
-
-import {
-	LanguageClient,
-	LanguageClientOptions,
-	ServerOptions,
-	TransportKind
-} from 'vscode-languageclient';
+import {commands, ExtensionContext, OutputChannel, workspace} from 'vscode';
+import {LanguageClient, LanguageClientOptions, ServerOptions, TransportKind} from 'vscode-languageclient';
+import * as WebSocket from 'ws';
 
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
-	console.log('Activating extension language-ttcn3');
+  console.log('Activating extension language-ttcn3');
 
-	let serverOptions: ServerOptions = {
-		run: { command: 'k3', args: ['langserver'] },
-		debug: { command: 'k3', args: ['langserver'] }
-	};
+  let serverOptions: ServerOptions = {
+    run: {command: 'k3', args: ['langserver']},
+    debug: {command: 'k3', args: ['langserver']}
+  };
 
-	let clientOptions: LanguageClientOptions = {
-		documentSelector: ['ttcn3']
-	};
+  const socketPort = workspace.getConfiguration('ttcn3').get('port', 7000);
+  let socket: WebSocket|null = null;
 
-	// Create the language client and start the client.
-	client = new LanguageClient('TTCN-3 Language Server', serverOptions, clientOptions);
-	client.registerProposedFeatures();
-	try {
-		context.subscriptions.push(client.start());
-	} catch (e) {
-		vscode.window.showErrorMessage('Could ot start the TTCN-3 language server.');
-	}
+  commands.registerCommand('ttcn3.startStreaming', () => {
+    socket = new WebSocket(`ws://localhost:${socketPort}`);
+  });
+
+  let log = '';
+  const websocketOutputChannel: OutputChannel = {
+    name: 'websocket',
+    // Only append the logs but send them later
+    append(value: string) {
+      log += value;
+      console.log(value);
+    },
+    appendLine(value: string) {
+      log += value;
+      // Don't send logs until WebSocket initialization
+      if (socket && socket.readyState === WebSocket.OPEN) {
+        socket.send(log);
+      }
+      log = '';
+    },
+    clear() {},
+    show() {},
+    hide() {},
+    dispose() {}
+  };
+
+  let clientOptions: LanguageClientOptions = {
+    documentSelector: ['ttcn3'],
+    // outputChannel: websocketOutputChannel
+  };
+
+  // Create the language client and start the client.
+  client = new LanguageClient(
+      'ttcn3', 'TTCN-3 Language Server', serverOptions, clientOptions);
+  client.registerProposedFeatures();
+  try {
+    context.subscriptions.push(client.start());
+  } catch (e) {
+    vscode.window.showInformationMessage('Could ot start the TTCN-3 language server. Please install k3-tool from http://github.com/nokia/ntt.')
+  }
 }
 
-export function deactivate(): Thenable<void> | undefined {
-	if (!client) {
-		return undefined;
-	}
-	return client.stop();
+export function deactivate(): Thenable<void>|undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
 }
